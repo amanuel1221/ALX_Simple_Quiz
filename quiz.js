a33a2b62da1d14895b98e8e1ce16deaa115a3d04
Amanuel <beginningoflife2324@gmail.com>
2025-08-01 07:40:21 -0700

    Updating the js part

--- quiz.js
+++ quiz.js
@@ -1,19 +1,20 @@
-function checkanswer(){
-   const correctAnswer = "4";
-   const selectedOption = document.querySelector("input[name='quiz']:checked");
+function checkAnswer() {
+    const correctAnswer = "4"; 
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+    const feedback = document.getElementById("feedback");
 
-   if(!selectedOption) {
+    if (!selectedOption) {
+        feedback.textContent = "Please select an answer.";
+        return;
+    }
 
-       document.getElementById("feedback").textContent = "Please select an answer.";
-       return;
-   }
     const userAnswer = selectedOption.value;
-    if(userAnswer === correctAnswer) {
-        document.getElementById("feedback").textContent = "Correct! Well done.";
-    }
-    else{
-        document.getElementById("feedback").textContent = "That's incorrect. Try again!";
-    }
 
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct! Well done."; 
+    } else {
+        feedback.textContent = "That's incorrect. Try again!"; 
+    }
 }
-document.getElementById("submit-answer").addEventListener("click", checkanswer);
+const submitButton = document.getElementById("submit-answer"); 
+submitButton.addEventListener("click", checkAnswer); 
